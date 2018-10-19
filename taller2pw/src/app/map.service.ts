c776d48891b3bd4a9cdbdca3e3d7132d3e30e5ee
bryancdn969 <bryan.cdn.20@gmail.com>
2018-10-19 13:10:29 -0400

    anges by defgault

--- taller2pw/src/app/map.service.ts
+++ taller2pw/src/app/map.service.ts
@@ -9,13 +9,8 @@ let apiUrl = 'http://192.168.1.22:8080';
 })
 export class MapService {
 
-  headers: any;
-  options: any;
-
-
   constructor(private http: Http) {
-    this.headers = new Headers({ 'Content-Type': 'application/json' });
-    this.options = new RequestOptions({ headers: this.headers });
+    this.http = http;
   }
 
   getAddress() {
@@ -23,8 +18,13 @@ export class MapService {
   }
 
   private callGetService(url: string, method: any) {
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Headers',  'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+    headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    headers.append('Access-Control-Allow-Credentials',  'true');
     return new Promise((resolve, reject) => {
-      this.http.get(apiUrl + url, this.options).toPromise()
+      this.http.get(apiUrl + url, {headers: headers}).toPromise()
         .then(res => resolve(res.json()),
           err => {
             console.log("callGetService =>" + method, err);
